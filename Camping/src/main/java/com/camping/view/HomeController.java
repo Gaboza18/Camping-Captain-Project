9aec5ed7ae7de5565cd3e073beafd27ffd9446f1
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2022-02-08 17:24:18 +0900

    first project upload

---
+++ Camping/src/main/java/com/camping/view/HomeController.java
@@ -0,0 +1,27 @@
+package com.camping.view;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	
+	
+	/**
+	 * index.html에서 메인화면 표시를 위한 index URL 요청처리
+	 */
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		return "index"; // index.jsp 화면을 호출
+	}
+	
+}
---
+++ Camping/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>테스트</title>
+</head>
+<body>
+	<h1>테스트 입니다</h1>
+</body>
+</html>
