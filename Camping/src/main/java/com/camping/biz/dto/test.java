7fd1ee4ce2ebfb3d0b48dffbc1aa2fd85e5b0c95
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2022-02-09 14:58:00 +0900

    sample date 추가

---
+++ Camping/src/main/java/com/camping/biz/dto/test.java
@@ -0,0 +1,5 @@
+package com.camping.biz.dto;
+
+public class test {
+
+}
---
+++ Camping/src/main/resources/mappings/test.java
@@ -0,0 +1,7 @@
+package com.camping.biz.dto;
+
+public class test {
+	private void sysout() {
+		System.out.println("example.xml -> Mybatis »ý¼º");
+	}
+}
